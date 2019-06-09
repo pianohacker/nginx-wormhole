442f3f0fb4c38b74f8d331cfcad4b62956e7492c
Jesse Weaver <pianohacker@gmail.com>
2019-06-09 02:55:17 -0600

    Add nginx reloading

--- nginx_wormhole/server.py
+++ nginx_wormhole/server.py
@@ -1,5 +1,6 @@
 import asyncio
 import asyncssh
+import collections
 import logging
 import os
 
@@ -9,12 +10,43 @@ from .common import VALID_SERVICE_NAMES
 conf = envconf.EnvConf(
 	NW_LISTEN_HOST = (str, 'localhost'),
 	NW_LISTEN_PORT = (int, 12322),
-	NW_LOG_LEVEL = (lambda value: value.upper(), 'info'),
+	NW_LOG_LEVEL = (lambda value: value.upper(), 'INFO'),
 	NW_HOST_KEY = (str, ''),
+
+	NW_NGINX_DIRECTORY = (str, '/etc/nginx/wormhole'),
+	NW_NGINX_RELOAD_COMMAND = (str, 'systemctl reload nginx.service'),
+	NW_DOMAIN = str,
+	NW_SSL_CERTIFICATE = str,
+	NW_SSL_PRIVATE_KEY = str,
 
 	print_report = True,
 )
 logger = logging.getLogger('nginx-wormhole')
+
+NGINX_COMMON_CONFIG = """listen 443 ssl;
+listen [::]:443 ssl;
+
+ssl on;
+ssl_certificate {NW_SSL_CERTIFICATE};
+ssl_certificate_key {NW_SSL_PRIVATE_KEY};
+
+ssl_protocols TLSv1.2 TLSv1.3;
+ssl_prefer_server_ciphers on;
+ssl_dhparam /etc/nginx/dhparam.pem;
+ssl_ciphers EECDH+AESGCM:EDH+AESGCM;
+ssl_ecdh_curve secp384r1;
+ssl_session_timeout  10m;
+ssl_session_cache shared:SSL:10m;
+ssl_session_tickets off;
+ssl_stapling on;
+ssl_stapling_verify on;
+resolver 8.8.8.8 8.8.4.4 valid=300s;
+resolver_timeout 5s;
+add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload";
+add_header X-Frame-Options DENY;
+add_header X-Content-Type-Options nosniff;
+add_header X-XSS-Protection "1; mode=block";
+"""
 
 def main():
 	logging.basicConfig(
@@ -22,6 +54,13 @@ def main():
 		level = conf.NW_LOG_LEVEL,
 	)
 	asyncssh.set_debug_level(2)
+
+	with open(
+		os.path.join(conf.NW_NGINX_DIRECTORY, 'common.conf'),
+		'w',
+		encoding = 'utf-8',
+	) as common_conf:
+		common_conf.write(NGINX_COMMON_CONFIG.format(**vars(conf)))
 
 	loop = asyncio.get_event_loop()
 
@@ -52,6 +91,40 @@ def get_or_generate_server_key():
 
 	return open('server_id_ed25519', 'rb').read()
 
+service_remote_ports = collections.defaultdict(lambda: set())
+
+async def update_nginx_conf(service_name):
+	if not service_remote_ports[service_name]:
+		os.unlink(os.path.join(conf.NW_NGINX_DIRECTORY, f'{service_name}.conf'))
+		return
+
+	with open(
+		os.path.join(conf.NW_NGINX_DIRECTORY, f'{service_name}.conf'),
+		'w',
+		encoding = 'utf-8',
+	) as service_conf:
+		service_conf.write(f"""server {{
+	server_name {service_name}.{conf.NW_DOMAIN};
+	include {conf.NW_NGINX_DIRECTORY}/common.conf;
+
+	location / {{
+		proxy_pass http://nginx_wormhole_{service_name.replace("-", "_")}_upstream;
+	}}
+}}
+
+upstream {{
+""")
+		for port in service_remote_ports[service_name]:
+			service_conf.write(f"	server localhost:{port};\n")
+
+		service_conf.write("}\n")
+	
+	proc = await asyncio.create_subprocess_exec(
+		*conf.NW_NGINX_RELOAD_COMMAND.split(),
+		stdin = asyncio.subprocess.DEVNULL,
+	)
+	await proc.wait()
+
 class WormholeServer(asyncssh.SSHServer):
 	def __init__(self):
 		self.conn = None
@@ -66,10 +139,14 @@ class WormholeServer(asyncssh.SSHServer):
 			return
 
 		logger.info(
-			'Removing forwards for service %s to ports %s',
+			'Removing forwards for service %s to port(s) %s',
 			self.service_name,
 			', '.join(str(port) for port in self.remote_ports),
 		)
+
+		service_remote_ports[self.service_name] -= self.remote_ports
+
+		asyncio.create_task(update_nginx_conf(self.service_name))
 
 	def begin_auth(self, service_name):
 		if VALID_SERVICE_NAMES.match(service_name):
@@ -94,6 +171,9 @@ class WormholeServer(asyncssh.SSHServer):
 
 		logger.info(f"Forwarding {self.service_name} from local port {listener.get_port()}")
 		self.remote_ports.add(listener.get_port())
+		service_remote_ports[self.service_name].add(listener.get_port())
+
+		await update_nginx_conf(self.service_name)
 
 		return listener
 
