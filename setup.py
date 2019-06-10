8e801a6565b8858f6b8281d1313cfa0dd9f5f953
Jesse Weaver <pianohacker@gmail.com>
2019-06-10 00:19:13 -0600

    Bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/pianohacker/nginx-wormhole'
 EMAIL = 'pianohacker@gmail.com'
 AUTHOR = 'Jesse Weaver'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
